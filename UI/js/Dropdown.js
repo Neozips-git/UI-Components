0b11bcffdc7bbdde60e8f92ab4ab8c839dcc2623
ourcrews <110927085+neozips-dev@users.noreply.github.com>
2023-03-01 07:56:16 -0500

    .

--- UI/js/Dropdown.js
+++ UI/js/Dropdown.js
@@ -5,12 +5,24 @@ document.addEventListener('click', () => {
 	dropdownAll.forEach((elem) => {
 		if(elem == dropdown) {
 			if(elem.classList.contains('keep-open') || event.target.closest('.keep-open')) {
-                elem.classList.add('open')
+				if(!elem.classList.contains('open')) {
+					elem.classList.add('open')
+					autoFocus(elem)
+				}
             }else{
                 elem.classList.toggle('open')
+				autoFocus(elem)
             }
 		}else{
 			elem.classList.remove('open')
 		}
     })
+
+	function autoFocus(elem) {
+		setTimeout(() => {
+			if(elem.classList.contains('open')) {
+				if(elem.querySelector('input[type=text]')) elem.querySelector('input[type=text]').focus()
+			}
+		}, 100)
+	}
 }, false)
